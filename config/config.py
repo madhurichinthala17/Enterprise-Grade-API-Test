16879ed3bb75ae30eb0825c162c07d87d683591d
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-11 17:34:40 -0600

    update pytest.ini

---
+++ config/config.py
@@ -0,0 +1,8 @@
+"""
+Config File for API Testing
+"""
+import os
+
+class Config:
+    BASEURL : os.getenv("BASE_URL","https://fakestoreapi.com") # pyright: ignore[reportInvalidTypeForm]
+    TIMEOUT : int(os.getenv("TIMEOUT","10")) # type: ignore
