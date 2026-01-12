74160ddd827cf5525386ab76b108cb8a6cd557de
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-11 18:26:52 -0600

    update config and pytest

--- config/config.py
+++ config/config.py
@@ -6,3 +6,8 @@ import os
 class Config:
     BASEURL : os.getenv("BASE_URL","https://fakestoreapi.com") # pyright: ignore[reportInvalidTypeForm]
     TIMEOUT : int(os.getenv("TIMEOUT","10")) # type: ignore
+
+    HEADERS ={
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+    }
