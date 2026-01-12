0cfb157a2ae6262ab2330a88de1b9ee6e8079a22
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-11 18:02:07 -0600

    add get and post requests

---
+++ utils/api_client.py
@@ -0,0 +1,41 @@
+from config.config import Config
+import requests
+
+class APIClient:
+    def __init__(self,base_url):
+        self.base_url= base_url or Config.BASEURL
+        self.timeout = Config.TIMEOUT
+        self.headers=Config.HEADERS.copy()
+        self.session=requests.sessions.Session()
+
+    def get(self,endpoint,headers=None,params=None):
+        url = f"{self.base_url}{endpoint}"
+        request_headers = self.headers.copy()
+        if headers:
+            request_headers.update(headers)
+        try:
+            response =self.session.get(url,
+                                       headers=request_headers,
+                                       timeout=self.timeout,
+                                       params=params)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(e)
+            raise
+
+    def post(self,endpoint,data=None,headers=None):
+        url=f"{self.base_url}{endpoint}"
+        request_headers=self.headers.copy
+        if headers:
+            request_headers=self.headers.update(headers)
+
+        try:
+            response=self.session.post(url,
+                                       json=data,
+                                       headers=request_headers,
+                                       timeout=self.timeout)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(e)
+            raise
+
