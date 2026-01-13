20b2a74bc0bc56dbe149bf085ff3fd3a1d1b4c27
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-12 18:26:47 -0600

    update response_validator

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 from utils.api_client import APIClient
+from validators.schema_validator import SchemaValidator
+from validators.response_validator import ResponseValidator
 
 
 @pytest.fixture(scope="class")
@@ -13,6 +15,8 @@ class Testproducts:
 
     def test_get_all_products(self,api_client):
         response = api_client.get("/products")
+        ResponseValidator.validate_status_code(response,200)
+        
 
    
 
