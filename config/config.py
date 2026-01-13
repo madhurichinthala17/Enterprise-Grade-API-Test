6dee7b75e63dac5c3ab9eda12fb6ab8b47e0745f
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-12 19:05:24 -0600

    get all products testcase

--- config/config.py
+++ config/config.py
@@ -4,8 +4,8 @@ Config File for API Testing
 import os
 
 class Config:
-    BASEURL : os.getenv("BASE_URL","https://fakestoreapi.com") # pyright: ignore[reportInvalidTypeForm]
-    TIMEOUT : int(os.getenv("TIMEOUT","10")) # type: ignore
+    BASEURL : str = os.getenv("BASE_URL","https://fakestoreapi.com") # pyright: ignore[reportInvalidTypeForm]
+    TIMEOUT : int = int(os.getenv("TIMEOUT","10")) # type: ignore
 
     HEADERS ={
         "Content-Type" : "application/json",
--- tests/test_products.py
+++ tests/test_products.py
@@ -11,12 +11,19 @@ def api_client():
     yield client
     client.close()
 
+@pytest.mark.products
 class Testproducts:
 
+    @pytest.mark.positive
     def test_get_all_products(self,api_client):
         response = api_client.get("/products")
         ResponseValidator.validate_status_code(response,200)
-        
+        ResponseValidator.validate_response_time(response,api_client.timeout)
+        ResponseValidator.validate_array_not_empty(response)
+        data =response.json()
+        for product in data:
+            assert SchemaValidator.Validate_product_schema(product)
+
 
    
 
