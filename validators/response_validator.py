8ac4828c200a8321c9c3c701c7466f02ae65509f
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-12 19:05:24 -0600

    get all products testcase

--- validators/response_validator.py
+++ validators/response_validator.py
@@ -5,3 +5,14 @@ class ResponseValidator:
     def validate_status_code(response,expected_statuscode):
         assert response.status_code == expected_statuscode
         return True
+    def validate_response_time(response,max_response_time):
+        response_time = response.elapsed.total_seconds()
+        assert response_time <= max_response_time
+        return True
+    def validate_array_not_empty(response):
+        try:
+            data =response.json()
+            assert isinstance(data,list) and len(data) >0
+            return True
+        except ValueError:
+            raise AssertionError("Response is not a valid JSON array")
