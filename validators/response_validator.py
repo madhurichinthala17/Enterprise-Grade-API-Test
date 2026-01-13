a90e59be9b65d154f2d636d6a22a35832b3d479f
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-12 19:52:49 -0600

    get positive and negative testcases for products

--- validators/response_validator.py
+++ validators/response_validator.py
@@ -5,10 +5,12 @@ class ResponseValidator:
     def validate_status_code(response,expected_statuscode):
         assert response.status_code == expected_statuscode
         return True
+    
     def validate_response_time(response,max_response_time):
         response_time = response.elapsed.total_seconds()
         assert response_time <= max_response_time
         return True
+    
     def validate_array_not_empty(response):
         try:
             data =response.json()
@@ -16,3 +18,13 @@ class ResponseValidator:
             return True
         except ValueError:
             raise AssertionError("Response is not a valid JSON array")
+        
+    def validate_empty_response(response):
+          # Check if content is empty
+        if not response.content:
+            return True
+        return False
+        
+    def validate_product_not_found(response):
+        ResponseValidator.validate_status_code(response,200)
+        ResponseValidator.validate_empty_response(response)
