cea7c6e74c38aea1f4af3f3cf9374fed396381be
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-12 18:26:47 -0600

    update response_validator

--- validators/response_validator.py
+++ validators/response_validator.py
@@ -1,4 +1,7 @@
 import time
 
 class ResponseValidator:
-    
+    #validate status code
+    def validate_status_code(response,expected_statuscode):
+        assert response.status_code == expected_statuscode
+        return True
