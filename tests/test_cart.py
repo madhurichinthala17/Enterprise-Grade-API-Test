5a3a72ac94e92cdfd68008663d6a3fab0f64a366
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-18 19:19:28 -0600

    get testcases for cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -22,3 +22,35 @@ class TestCarts:
         ResponseValidator.validate_response_time(response,api_client.timeout)
         SchemaValidator.Validate_cart_schema(response.json()[0])
         
+
+    @pytest.mark.positive
+    def test_get_cart_by_id(self,api_client):
+        response =api_client.get("/carts/1")
+        ResponseValidator.validate_status_code(response,200)
+        data =response.json()
+        SchemaValidator.Validate_cart_schema(data)
+        assert data["id"] ==1
+
+    @pytest.mark.positive
+    def test_get_carts_for_user(self,api_client):
+        user_id =1
+        response=api_client.get(f"/carts/user/{user_id}")
+        ResponseValidator.validate_status_code(response,200)
+        data =response.json()
+        for cart in data:
+            SchemaValidator.Validate_cart_schema(cart)
+            assert cart["userId"] == user_id
+
+    @pytest.mark.negative
+    def test_get_cart_by_invalid_id(self,api_client):
+        ids =["abc","!@#"]
+        for cart_id in ids:
+            response=api_client.get(f"/carts/{cart_id}")
+            ResponseValidator.validate_invalid_response(response)
+
+    @pytest.mark.negative
+    def test_get_carts_for_invalid_user(self,api_client):
+        ids =["xyz","$%^"]
+        for user_id in ids:
+            response=api_client.get(f"/carts/user/{user_id}")
+            ResponseValidator.validate_invalid_response(response)
