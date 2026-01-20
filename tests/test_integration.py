287b7386c525b87142ea723e34b152dc5d25e5ce
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-01-19 19:34:43 -0600

    integration tc2

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -39,3 +39,30 @@ class TestIntegration:
         ResponseValidator.validate_status_code(cart_response,201)
         cart =cart_response.json()
         assert cart["userId"] == user_id
+
+    @pytest.mark.positive
+    def test_product_cart_integration(self,api_client):
+
+        #Fetch a product
+        response =api_client.get("/products/1")
+        ResponseValidator.validate_status_code(response,200)
+        product =response.json()
+        product_id =product["id"]
+
+
+        #Create a cart with the product
+        cart_data ={
+            "userId": 1,
+            "date": "2023-10-10",
+            "products": [
+                {"productId": product_id, "quantity": 3}
+            ]
+        }
+        cart_response =api_client.post("/carts",data=cart_data)
+        ResponseValidator.validate_status_code(cart_response,201)
+        cart =cart_response.json()
+        products_in_cart =cart["products"]
+        for product in products_in_cart:
+            assert product["productId"] == product_id
+
+
